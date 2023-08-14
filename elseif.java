0cdac8e8c210ee83b98a06d7dd3c32315878a38a
gen.java <132185718+GrizonRussell@users.noreply.github.com>
2023-08-14 20:16:34 +0800

    elseif.java.OOP

---
+++ elseif.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class elseif{
+    public static void main(String[] args){
+    int name = 4;
+    if (name < 2) {
+        System.out.println("mel");
+      } else if (name < 3) {
+        System.out.println("angelo");
+      } else {
+        System.out.println("macario");
+      }
+    }
+}
